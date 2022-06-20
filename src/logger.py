62e6e6a62fd2d92c846fc14c0e85169286d3fc61
Machalka <al1999dk@gmail.com>
2022-06-21 01:29:51 +0300

    before push

---
+++ src/logger.py
@@ -0,0 +1,8 @@
+import logging
+
+FORMAT = '%(asctime)s %(clientip)-15s %(user)-8s %(message)s'
+logging.basicConfig(format=FORMAT)
+d = {'clientip': '192.168.0.1', 'user': 'fbloggs'}
+# logger = logging.getLogger('tcpserver')
+logger = logging.getLogger()
+logger.error('Protocol problem: %s', 'connection reset', extra=d)
